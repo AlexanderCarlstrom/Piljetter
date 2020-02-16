2f0343a8d2b298549b58f20075255a6115a37529
Alexander Carlström <caarlstrom.alexander@gmail.com>
2020-02-16 14:32:40 +0100

    Fixed db connection

--- server/index.js
+++ server/index.js
@@ -1,22 +1,21 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const dotenv = require('dotenv');
-const mysql = require('mysql');
+const mysql = require('@mysql/xdevapi');
 const app = express();
 dotenv.config();
 
 const port = 3000;
-
-const db = mysql.createConnection({
+const config = {
   host: 'localhost',
-  user: 'root',
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASS,
-  database: 'piljetter'
-});
+  database: 'piljetter',
+  port: process.env.DB_PORT
+};
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('connected to db');
+mysql.getSession(config).then(session => {
+  console.log(session.inspect());
 });
 
 app.use(bodyParser.json());
